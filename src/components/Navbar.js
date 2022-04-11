5ce25b1c0fc825b5366038e4a394bab2b23c2746
unknown <Aswin20010>
2022-04-11 22:30:27 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Navbar = () =>{
+    return(
+        <>
+            <nav>
+                <h3></h3>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
